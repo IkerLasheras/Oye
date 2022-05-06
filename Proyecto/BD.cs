9b88057b48d0e854a54f98a62c7b8fefedc5009f
IkerLasheras <lasherasiker@gmail.com>
2022-05-06 19:50:53 +0200

    Agregar archivos de proyecto.

---
+++ Proyecto/BD.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Proyecto
+{
+    public class BD
+    {
+        public static SqlConnection CrearConexion(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            return connection;
+        }
+
+        public static void AbrirConexion(SqlConnection connection, SqlCommand command)
+        {
+            command.Connection.Open();
+        }
+        public static void CerrarConexion(SqlConnection connection)
+        {
+            connection.Close();
+        }
+        public static SqlCommand CrearComando(SqlConnection connection, string script)
+        {
+            return new SqlCommand(script, connection);
+        }
+        public static SqlDataAdapter CrearDataAdapter(SqlCommand command)
+        {
+            return new SqlDataAdapter(command);
+        }
+
+        public static DataTable CrearDataTable(SqlDataAdapter da)
+        {
+            DataTable dt = new DataTable();
+            return dt;
+        }
+
+        public static DataTable RellenarDataTable(DataTable dt, string script, string connectionString)
+        {
+            SqlConnection connection = CrearConexion(connectionString);
+            SqlCommand command = CrearComando(connection, script);
+
+            SqlDataAdapter da = CrearDataAdapter(command);
+            dt = CrearDataTable(da);
+
+            AbrirConexion(connection, command);
+
+            da.Fill(dt);
+
+            return dt;
+        }
+
+        public static SqlParameter ObtenerParametro(string stringParametro, SqlDbType tipoParametro, ParameterDirection direccion, bool valorNulo, Object valor)
+        {
+            SqlParameter parametro = new SqlParameter(stringParametro, tipoParametro);
+            parametro.IsNullable = valorNulo;
+            parametro.Direction = direccion;
+            parametro.Value = valor;
+            return parametro;
+        }
+
+        public static void LanzarComandoSQLNonQuery(string cadenaSQL, string cadenaConexion, List<SqlParameter> listaParametros)
+        {
+            //try
+            //{
+            SqlConnection conexion = CrearConexion(cadenaConexion);
+            SqlCommand comando = CrearComando(conexion, cadenaSQL);
+            AbrirConexion(conexion, comando);
+            AniadirParametroComandos(comando, listaParametros);
+            EjecutarQuery(comando);
+
+            CerrarConexion(conexion);
+            //}
+            //catch (Exception e)
+            //{
+            //  return e;
+            // }
+            //return null;
+        }
+        public static void AniadirParametroComandos(SqlCommand comando, List<SqlParameter> listaParametros)
+        {
+            for (int i = 0; i < listaParametros.Count; i++)
+            {
+                comando.Parameters.Add(listaParametros[i]);
+            }
+        }
+        /// <returns></returns>
+        public static int EjecutarQuery(SqlCommand comando)
+        {
+            int numeroFilas = 0;
+
+            //try
+            //{
+            numeroFilas = comando.ExecuteNonQuery();
+            //}
+            //catch (Exception e)
+            //{
+            // Debug.WriteLine("Error ejecutando el Query");
+            //}
+            return numeroFilas;
+        }
+    }
+
+}
+
---
+++ ProyectoVerano2/WinInsertarEmpleados.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Proyecto;
+using System.IO;
+using Windows.Storage;
+using Microsoft.Win32;
+
+namespace ProyectoVerano2
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class WinInsertarEmpleados : Window
+    {
+
+        public WinInsertarEmpleados()
+        {
+            InitializeComponent();
+            BuscarIdMax("idEmpleado", "dbo.Empleados");
+
+        }
+        private void btnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAnterior_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        public void BuscarIdMax(string columna, string tabla)
+        {
+            int id = 0;
+
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["proyecto"].ConnectionString;
+            string script = "SELECT Max(idEmpleado) FROM dbo.Empleados";
+            DataTable dt = new DataTable();
+            BD.RellenarDataTable(dt, script, connectionString);
+
+            if (!dt.HasErrors)
+            {
+                id = 0;
+            }
+            else
+            {
+                DataRow row = dt.Rows[0];
+                id = (int)row["idEmpleado"];
+            }
+            txtID.Text = id.ToString();
+
+        }
+        public void InsertarDatosBBDD()
+        {
+            string comandMod =
+                "INSERT INTO [dbo].[Empleados] " +
+                "([nombre]," +
+                "[apellido1]," +
+                "[apellido2]," +
+                "[dni]," +
+                "[telefono]" +
+                ",[email]," +
+                "[fechNac]" +
+                ",[localidad]" +
+                ",[direccion]" +
+                ",[cp]" +
+                ",[tipoEmpleado]" +
+                ",[cuentaCredito]" +
+                ",[fechIncicioContrato]" +
+                ",[imgEmpleado])" +
+                "VALUES" +
+                "(@nombre" +
+                ", @apellido1" +
+                ", @apellido2" +
+                ", @dni" +
+                ", @telefono" +
+                ", @email" +
+                ", @fechNac" +
+                ", @localidad" +
+                ", @direccion" +
+                ", @cp" +
+                ", @tipoEmpleado" +
+                ", @cuentaCredito" +
+                ", @fechIncicioContrato" +
+                ", @imgEmpleado);";
+
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["proyecto"].ConnectionString;
+
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+
+            string[] apellidos = txtApellidos.Text.Split(' ');
+            string apellido2 = "";
+
+            if (apellidos.Length != 0)
+            {
+                apellido2 = "apellido[1]";
+            }
+            DateTime fechaNacimiento = DateTime.ParseExact(txtFechNac.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DateTime fechaInicio = DateTime.ParseExact(txtFechInicio.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            listaParametros.Add(BD.ObtenerParametro("@nombre", SqlDbType.NChar, ParameterDirection.Input, false, txtNombre.Text));
+            listaParametros.Add(BD.ObtenerParametro("@apellido1", SqlDbType.NChar, ParameterDirection.Input, false, apellidos[0]));
+            listaParametros.Add(BD.ObtenerParametro("@apellido2", SqlDbType.NChar, ParameterDirection.Input, true, apellido2));
+            listaParametros.Add(BD.ObtenerParametro("@dni", SqlDbType.NChar, ParameterDirection.Input, false, txtDni.Text));
+            listaParametros.Add(BD.ObtenerParametro("@telefono", SqlDbType.NChar, ParameterDirection.Input, true, txtMovil.Text));
+            listaParametros.Add(BD.ObtenerParametro("@email", SqlDbType.NChar, ParameterDirection.Input, true, txtEmail.Text));
+            listaParametros.Add(BD.ObtenerParametro("@fechNac", SqlDbType.Date, ParameterDirection.Input, true, fechaNacimiento));
+            listaParametros.Add(BD.ObtenerParametro("@localidad", SqlDbType.NChar, ParameterDirection.Input, true, txtLocalidad.Text));
+            listaParametros.Add(BD.ObtenerParametro("@direccion", SqlDbType.NChar, ParameterDirection.Input, true, txtDireccion.Text));
+            listaParametros.Add(BD.ObtenerParametro("@cp", SqlDbType.Int, ParameterDirection.Input, true, 50014));
+            listaParametros.Add(BD.ObtenerParametro("@tipoEmpleado", SqlDbType.Int, ParameterDirection.Input, true, 1));
+            listaParametros.Add(BD.ObtenerParametro("@cuentaCredito", SqlDbType.NChar, ParameterDirection.Input, false, txtFIBAN.Text));
+            listaParametros.Add(BD.ObtenerParametro("@fechIncicioContrato", SqlDbType.Date, ParameterDirection.Input, true, fechaInicio));
+            listaParametros.Add(BD.ObtenerParametro("@imgEmpleado", SqlDbType.Image, ParameterDirection.Input, false, imgEmpleado));
+
+            BD.LanzarComandoSQLNonQuery(comandMod, connectionString, listaParametros);
+
+            Close();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            InsertarDatosBBDD();
+        }
+
+        private async void btnInsertarImg_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Image files | *.jpg";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    txtStudentImage.Text = openFileDialog1.FileName;
+                    pbStudentImage.Image = Image.FromFile(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+    }
+
+}
