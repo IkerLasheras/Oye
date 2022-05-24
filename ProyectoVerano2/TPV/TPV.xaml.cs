5c5ae8372c0d8387390e823e46b002904c895609
Iker Lasheras <aa20.ilasheras29@iespabloserrano.com>
2022-05-24 15:41:34 +0200

    cooo

--- ProyectoVerano2/TPV/TPV.xaml.cs
+++ ProyectoVerano2/TPV/TPV.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,62 +21,114 @@ namespace ProyectoVerano2.TPV
     /// </summary>
     public partial class TPV : Window
     {
-        public IList<Button> BotonesTipos{ get; set; }
-        public IList<Button> BotonesCategorias { get; set; }
-        public IList<Button> BotonesPlatos { get; set; }
+
+        public static string script = "";
+        public static bool nose = true;
+        public IList<Button> SubTipoBotones { get; set; }
+        public IList<Button> TipoBotones { get; set; }
+
+
+
         public TPV()
         {
-            crearBotonesPlatos();
-            //crearBotonesCategoria();
-            InitializeComponent();
-            
-
+            if(!nose )
+            {
+                CrearTipoBotones(script);
+            }
+           
+           
+                    InitializeComponent();
         }
-        public void crearBotonesPlatos()
-        {
-            BotonesPlatos = new List<Button>();
 
-            for (int i = 0; i < 10; i++)
+        public void CrearSubTipoBotones()
+        {
+            SubTipoBotones = new List<Button>();
+
+            for (int i = 0; i < 15; i++)
             {
 
 
-                BotonesPlatos.Add(new Button
-                {
+                Button btnSubTipo = new Button();
+                btnSubTipo.Content = i;
+                btnSubTipo.Width = 150;
+                btnSubTipo.Height = 50;
+                btnSubTipo.Click += btn_click;
 
-                    Name = "btn" + i.ToString(),
-                    Width = 200,
-                    Height = 100,
-                    Margin = new Thickness(10),
+                SubTipoBotones.Add(btnSubTipo);
 
-                });
-
-               DataContext = this;
+                DataContext = this;
             }
+            this.AddChild(TipoBotones);
         }
 
-        //public void crearBotonesCategoria()
-        //{
-        //    BotonesCategorias = new List<Button>();
-
-        //    for (int i = 0; i < 5; i++)
-        //    {
-
-
-        //        BotonesCategorias.Add(new Button
-        //        {
-
-        //            Name = "btn" + i.ToString(),
-        //            Width = 200,
-        //            Height = 100,
-        //            Margin = new Thickness(10),
-
-        //        });
-
-         
-        //    }
-        //}
-        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        public void CrearTipoBotones(string script)
         {
+            TipoBotones = new List<Button>();
+
+            DataTable dt = new DataTable();
+            dt = BD.RellenarDataTable(dt, script);
+
+            for (int i = 0;i <1; i++)
+            {
+                Button btnTipo = new Button();
+                btnTipo.Content = i;
+                btnTipo.Width = 150;
+                btnTipo.Height = 50;
+                btnTipo.Click += btnTipo_click;
+
+                TipoBotones.Add(btnTipo);
+
+                DataContext = this;
+            }
+           
+        }
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+
+            int[] datos = { 1, 5, 7, 8, 10 };
+            Button button = (Button)sender;
+            int content = Convert.ToInt32(button.Content);
+            for (int i = 0; i < datos.Length; i++)
+            {
+                if (content == datos[i])
+                {
+                    button.Content = "aaaaaaa";
+                }
+            }
+
+
+        }
+        private void btnTipo_click(object sender, RoutedEventArgs e)
+        {
+
+            int[] datos = { 1, 5, 7, 8, 10 };
+            Button button = (Button)sender;
+            int content = Convert.ToInt32(button.Content);
+            for (int i = 0; i < datos.Length; i++)
+            {
+                if (content == datos[i])
+                {
+                    button.Content = "aaaaaaa";
+                }
+            }
+
+
+        }
+
+      
+        private void btnPlatos_Click(object sender, RoutedEventArgs e)
+        {
+            script = "select Nombretipo from tipoPlatos";
+            nose = false;
+            Window tpv = new TPV();
+        }
+
+        private void btnBebidas_Click(object sender, RoutedEventArgs e)
+        {
+            script = "select Nombre from tipoBebida";
+            nose = false;
+            Window tpv = new TPV();
 
         }
     }
